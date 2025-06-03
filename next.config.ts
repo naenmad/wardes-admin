6502993c3ec1a803b9085291157540873115e0ea
Ahmad Zulkarnaen <2310631170064@student.unsika.ac.id>
2025-06-03 11:06:18 +0700

    feat: Update Next.js configuration to enable experimental features and adjust image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['firebasestorage.googleapis.com', 'imgur.com', 'i.imgur.com']
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
